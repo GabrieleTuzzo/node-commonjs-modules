53de1426b041e5806725590b7edef074e8478180
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-15 17:39:23 +0100

    ADD data gather from input

--- people.js
+++ people.js
@@ -1,12 +1,31 @@
 const { newPerson } = require('./names');
 const { newHobbiesArray } = require('./hobbies');
 
-(function populatePerson() {
-    const user = newPerson('Gabriele', 'Tuzzo');
-    console.log(user);
-    const { hobbies } = newHobbiesArray('Canto', 'Chitarra', 'GameDev');
-    console.log(hobbies);
+function populatePerson(userObj) {
+    const user = newPerson(userObj.firstName, userObj.lastName);
+    // console.log(user);
+    const { hobbies } = newHobbiesArray(
+        userObj.hobbieOne,
+        userObj.hobbieTwo,
+        userObj.hobbieThree
+    );
+    // console.log(hobbies);
 
     user.hobbies = hobbies;
-    console.log(user);
-})();
+    return user;
+}
+
+const userData = process.argv.slice(2);
+// console.log(userData);
+const userObj = {
+    firstName: userData[0],
+    lastName: userData[1],
+    hobbieOne: userData[2],
+    hobbieTwo: userData[3],
+    hobbieThree: userData[4],
+};
+// console.log(userObj);
+
+const newUser = populatePerson(userObj);
+
+console.log('New user created: ', newUser);
